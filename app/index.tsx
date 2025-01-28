e7e9db7fbba61f8516d8d7a2977df7491228f8ef
marcosviana91 <marcos.viana.91@gmail.com>
2025-01-27 23:26:55 -0300

    feat - load screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { RootReducer } from "@/store";
@@ -8,29 +8,48 @@ import { getData } from '@/store/database';
 
 import Home from "@/pages/home";
 import Game from "@/pages/game";
+import FullLoading from "@/components/general/Loading";
 // import Table from "@/components/Table";
 
 export default function App() {
     const dispatch = useDispatch();
     const appData = useSelector((state: RootReducer) => state.appReducer.data)
-    const [getUser, { data: userDataAPI, error: userError }] = useGetAuthUserDataMutation();
+    const [getUser, { data: userDataAPI, isLoading }] = useGetAuthUserDataMutation();
+    const [loading, setLoading] = useState(true);
 
 
     useEffect(() => {
+        console.log("inicio:", Date.now());
         getData('userDataAuth').then(_data => {
             if (_data) {
                 getUser(_data)
+                setLoading(false);
             }
         })
     }, [])
 
     useEffect(() => {
+        console.log("userDataAPI", userDataAPI);
         if (userDataAPI) {
+            setLoading(isLoading);
+        }
+        if (userDataAPI && userDataAPI.id) {
             dispatch(login({
                 data: userDataAPI
             }))
+            console.log("fim LOGADO:", Date.now());
+            setLoading(isLoading);
+        } else {
+            // TODO: aviso de não logado
+            console.log("fim NÃO:", Date.now());
+            setLoading(isLoading);
         }
     }, [userDataAPI])
+
+    if (loading) {
+        return <FullLoading />
+    }
+
     switch (appData.page) {
         case 'game':
             return (
--- components/GameDetails.tsx
+++ components/GameDetails.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { View, Text, Image, Pressable, Linking } from "react-native";
+import { View, Text, Image, Pressable, Linking, Platform, ActivityIndicator } from "react-native";
 
 import { RootReducer } from "@/store";
 import { useGetGameDetailsQuery, useGetGameDLCQuery } from "@/store/api";
@@ -8,6 +8,7 @@ import { setPage, setSelectedGame } from "@/store/reducers/appReducer";
 
 import { getGameMediaFiles, checkGameMediaFiles } from "@/utils/FileSystem/Media";
 import { ThemedText } from "./themed/ThemedText";
+import FullLoading from "./general/Loading";
 
 const SITE = process.env.EXPO_PUBLIC_SITE_URL;
 
@@ -16,27 +17,32 @@ export default function GameDetails(props: { game_id: string }) {
     const appData = useSelector((state: RootReducer) => state.appReducer.data)
     const { data: game_data } = useGetGameDetailsQuery(props.game_id)
     const { data: game_dlc } = useGetGameDLCQuery(props.game_id)
-    const [gameReady, setGameReady] = useState<Boolean>(false);
+    const [gameReady, setGameReady] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         if (game_data) {
             dispatch(setSelectedGame(game_data))
+            setLoading(false)
         }
     }, [game_data])
 
     useEffect(() => {
         if (game_dlc) {
-            checkGameMediaFiles(game_dlc).then(files => {
+            console.log("Checando arquivos locais")
+            Platform.OS === 'android' && checkGameMediaFiles(game_dlc).then(files => {
                 setGameReady(Boolean(files.length === 0))
+                console.log("Arquivos locais checados")
             })
         }
     }, [game_dlc])
 
     return (
-        <View>
+        <View style={{ height: '85%' }}>
+            {loading && <FullLoading />}
             {appData.selected_game_details &&
-                <View>
-                    <ThemedText type="title" style={{ alignSelf: 'center', marginBottom: 12, fontWeight: 700, marginTop:12 }}>{appData.selected_game_details.title}</ThemedText>
+                <>
+                    <ThemedText type="title" style={{ alignSelf: 'center', marginBottom: 12, fontWeight: 700, marginTop: 12 }}>{appData.selected_game_details.title}</ThemedText>
                     <View style={{ flexDirection: 'row', padding: 12, gap: 8 }}>
                         <Image
                             style={{ width: 200, height: 200 }}
@@ -45,38 +51,16 @@ export default function GameDetails(props: { game_id: string }) {
                         <ThemedText style={{ flex: 1, textAlign: 'justify', fontSize: 18 }}>{appData.selected_game_details.description}</ThemedText>
 
                     </View>
-                    <Pressable
-                        onPress={() => {
-                            Linking.canOpenURL(`${SITE}/games/${props.game_id}`).then(() => {
-                                Linking.openURL(`${SITE}/games/${props.game_id}`)
-                            })
-                        }}
-                    >
-                        <View style={{
-                            backgroundColor: '#060',
-                            padding: 8,
-                            margin: 8,
-                            borderColor: '#fff',
-                            borderWidth: 2,
-                            borderRadius: 8,
-                        }}>
-                            <Text style={{
-                                fontSize: 16,
-                                fontWeight: 700,
-                                textAlign: "center",
-                            }}>Ver mais detalhes</Text>
-                        </View>
-                    </Pressable>
-                    {!gameReady
-                        ? <Pressable
+                    <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-evenly', position: 'absolute', bottom: 20 }}>
+                        <Pressable
                             onPress={() => {
-                                if (game_dlc) {
-                                    getGameMediaFiles(game_dlc).then(ready => setGameReady(ready))
-                                }
+                                Linking.canOpenURL(`${SITE}/games/${props.game_id}`).then(() => {
+                                    Linking.openURL(`${SITE}/games/${props.game_id}`)
+                                })
                             }}
                         >
                             <View style={{
-                                backgroundColor: '#0f0',
+                                backgroundColor: '#060',
                                 padding: 8,
                                 margin: 8,
                                 borderColor: '#fff',
@@ -87,31 +71,66 @@ export default function GameDetails(props: { game_id: string }) {
                                     fontSize: 16,
                                     fontWeight: 700,
                                     textAlign: "center",
-                                }}>Baixar Jogo</Text>
+                                }}>Ver mais detalhes</Text>
                             </View>
                         </Pressable>
-                        : <Pressable
-                            onPress={() => {
-                                dispatch(setPage('game'))
-                            }}
-                        >
-                            <View style={{
-                                backgroundColor: '#0f0',
-                                padding: 8,
-                                margin: 8,
-                                borderColor: '#fff',
-                                borderWidth: 2,
-                                borderRadius: 8,
-                            }}>
-                                <Text style={{
-                                    fontSize: 16,
-                                    fontWeight: 700,
-                                    textAlign: "center",
-                                }}>Jogar</Text>
-                            </View>
-                        </Pressable>
-                    }
-                </View>
+                        {!gameReady
+                            ? <Pressable
+                                disabled={loading}
+                                style={{ width: '50%' }}
+                                onPress={() => {
+                                    setLoading(true)
+                                    if (game_dlc) {
+                                        getGameMediaFiles(game_dlc).then(ready => {
+                                            setGameReady(ready)
+                                            setLoading(false)
+                                        })
+                                    }
+                                }}
+                            >
+
+                                <View style={{
+                                    backgroundColor: loading ? '#00ff0033' : '#0f0',
+                                    padding: 8,
+                                    margin: 8,
+                                    borderColor: '#fff',
+                                    borderWidth: 2,
+                                    borderRadius: 8,
+                                }}>
+                                    {loading
+                                        ? (<ActivityIndicator />)
+                                        : <Text style={{
+                                            fontSize: 16,
+                                            fontWeight: 700,
+                                            textAlign: "center",
+                                        }}>Baixar Jogo</Text>
+                                    }
+
+                                </View>
+                            </Pressable>
+                            : <Pressable
+                                onPress={() => {
+                                    dispatch(setPage('game'))
+                                }}
+                            >
+                                <View style={{
+                                    backgroundColor: '#0f0',
+                                    padding: 8,
+                                    margin: 8,
+                                    borderColor: '#fff',
+                                    borderWidth: 2,
+                                    borderRadius: 8,
+                                }}>
+                                    <Text style={{
+                                        fontSize: 16,
+                                        fontWeight: 700,
+                                        textAlign: "center",
+                                    }}>Jogar</Text>
+                                </View>
+                            </Pressable>
+                        }
+                    </View>
+                </>
             }
         </View>
     )
